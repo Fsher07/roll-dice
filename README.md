7bef9411c47dc8fb4547a88a20203332e1052f67
Fsher07 <furkansalimm@gmail.com>
2022-03-27 13:45:45 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# roll-dice
+ A rolling-dice game. If you can guess one the dices it returns "too close", if you can guess both it returns "Congratulations". 
---
+++ zarAtma.js
@@ -0,0 +1,88 @@
+
+
+function getDeger(button){
+    
+       var guess1 = parseInt(button.getAttribute("data-tip"));
+       this.guess1 = guess1 ;
+       document.getElementsByClassName("row")[(parseInt(button.getAttribute("data-tip"))-1)]
+       .style.backgroundColor = "blue";
+       console.log(guess1);
+        
+}
+
+function getDeger2(button){
+    
+    var guess2 = parseInt(button.getAttribute("data-tip"));
+    this.guess2 = guess2;
+    document.getElementsByClassName("row2")[(parseInt(button.getAttribute("data-tip"))-1)].style.backgroundColor = "blue";
+    console.log(guess2);
+
+}
+
+
+
+function resim_ekle() { 
+    document.getElementById("trollaciklama").innerHTML = "Not Today !";
+    document.getElementById("resim").innerHTML = "<img src='ohNo.jpg'>"; 
+}
+
+function rollDice(element){
+    
+
+    var random1 = Math.floor((Math.random()*6)+1);
+    var random2 = Math.floor((Math.random()*6)+1);
+
+    
+    document.getElementById("rolling").innerHTML = "Rolling...";
+    document.getElementById("dice_place1").innerHTML ="<img src=" +  random1 + ".png>";
+    document.getElementById("dice_place2").innerHTML = "<img src=" +  random2 + ".png>";
+    
+    var array = []; 
+    array.push(random1, random2);
+    console.log(array);
+    
+    var a = array.includes(guess1);
+    var b = array.includes(guess2);    
+      
+    
+
+   
+    
+    if (guess1 == guess2){
+        let x = Boolean(guess1 == array[0]);
+        let y = Boolean(guess1 == array[1]);
+
+        
+        
+        if(x && y ){
+            console.log("tombila");
+
+        }else if(x || y){
+            console.log("was very close!!");
+
+        }else console.log("god dam it ");
+    }
+
+    if(guess1 != guess2){
+        
+         if(a  && b  ){
+    
+            console.log("Tombala !");
+        
+        }else if(a  || b ){
+        
+            console.log("was close !");
+            
+        }else resim_ekle(element);
+    
+
+
+
+
+    }
+    
+      
+   
+        
+
+    } 
